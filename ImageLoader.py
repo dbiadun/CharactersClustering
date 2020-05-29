be2e8bf32b2751d09cca061e60615009b5e06e6b
dawid <dawidbiadun12@gmail.com>
2020-05-29 18:32:53 +0200

    Get rid of redundant Runner class

--- ImageLoader.py
+++ ImageLoader.py
@@ -8,6 +8,16 @@ class ImageLoader:
     """
 
     @staticmethod
-    def load_image(path):
+    def load_images(paths_file):
+        paths = []
+        with open(paths_file, 'r') as fh:
+            for line in fh:
+                paths.append(line[:-1])
+
+        paths_list = [ImageLoader._load_single_image(path) for path in paths]
+        return paths_list
+
+    @staticmethod
+    def _load_single_image(path):
         image_array = io.imread(path)
         return Image(image_array, path)
--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 import sys
 
-from Runner import Runner
+from ImageLoader import ImageLoader
+from ClusteringHandler import ClusteringHandler
+from HTMLCreator import HTMLCreator
+import constants as c
 
 if len(sys.argv) < 2:
     print("Please pass a path to a file containing image paths list")
 else:
-    Runner.run('image_paths')
+    paths_file = sys.argv[1]
+    images = ImageLoader.load_images(paths_file)
+    clustered_images = ClusteringHandler.cluster(images)
+    HTMLCreator.create_html(c.HTML_TEMPLATE, c.HTML_OUTPUT, clustered_images=clustered_images,
+                            clusters_count=len(clustered_images))
+
