979abeede1075b7ced5bbd22f46ac2de94656207
dawid <dawidbiadun12@gmail.com>
2020-05-29 16:10:44 +0200

    Set reading all images instead of some constant

--- Runner.py
+++ Runner.py
@@ -12,7 +12,8 @@ class Runner:
     def run(paths_file):
         images = Runner._load_images(paths_file)
         clustered_images = ClusteringHandler.cluster(images)
-        HTMLCreator.create_html('template.html', 'index.html', clustered_images=clustered_images, clusters_count=len(clustered_images))
+        HTMLCreator.create_html('template.html', 'index.html', clustered_images=clustered_images,
+                                clusters_count=len(clustered_images))
 
     @staticmethod
     def _load_images(paths_file):
@@ -21,5 +22,5 @@ class Runner:
             for line in fh:
                 paths.append(line[:-1])
 
-        paths_list = [ImageLoader.load_image(path) for path in paths[:1000]]
+        paths_list = [ImageLoader.load_image(path) for path in paths]
         return paths_list
